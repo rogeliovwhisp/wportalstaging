c148492a7c556345878bb4dc306af0541d486b6a
rogeliovwhisp <119999827+rogeliovwhisp@users.noreply.github.com>
2023-01-12 08:46:32 -0800

    polish collapse sidebar updates
    polish collapse sidebar updates

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
  $(document).ready(function(){
           var boxWidth = $(".box").width();
           $(".slide-right").hide();
+          $(".rightLogo .logoCollapse").hide();
+          
 
           $(".slide-left").click(function(){
+              $(".rightLogo .logo").hide();
               $(".leftSideBar").animate({
                   width: "5%"
               });
@@ -16,9 +19,17 @@
               $(".slide-left").hide();
               $(".sb_text").hide();
               $(".slide-right").show();
+              $(".leftSideBar .fixed-bottom").css("margin-left","-10px");
+              $(".leftSideBar .outlogin").css("margin-left","-12px");
+              $(".rightLogo .logoCollapse").show();
+              $(".leftSideBar .leftNavigation li a").css("text-align","center");
+              $(".leftSideBar .leftNavigation .icon_item").css("padding-left","0px");
+
+              
           });
 
           $(".slide-right").click(function(){
+              $(".rightLogo .logo").show();
               $(".leftMainWrap").animate({
                   width: "83%"
               });
@@ -31,6 +42,11 @@
               $(".slide-right").hide();
               $(".sb_text").fadeIn('slow').animate({opacity: 1.0});
               $(".slide-left").show();
+              $(".leftSideBar .fixed-bottom").css("margin-left","0px");
+              $(".leftSideBar .outlogin").css("margin-left","0px");
+              $(".rightLogo .logoCollapse").hide();
+              $(".leftSideBar .leftNavigation li a").css("text-align","left");
+              $(".leftSideBar .leftNavigation .icon_item").css("padding-left","20px");
           });
       });
 // End of Global Left Navigator full screen sidebar Collapsible function//
