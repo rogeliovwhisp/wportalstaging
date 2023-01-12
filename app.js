82c7b9a6c2edd9c3582778b21180c8d297c7bf65
rogeliovwhisp <119999827+rogeliovwhisp@users.noreply.github.com>
2023-01-11 21:06:20 -0800

    collapse sidebar update
    collapse sidebar update

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 //Global Left Navigator full screen sidebar Collapsible function//
-$(document).ready(function(){
-    var boxWidth = $(".box").width();
-    $(".slide-right").hide();
+ $(document).ready(function(){
+          var boxWidth = $(".box").width();
+          $(".slide-right").hide();
 
-    $(".slide-left").click(function(){
-        $(".box").animate({
-            width: 60
-        });
-        $(".outlogin").animate({
-            width: "30%"
-        });
-        $(".slide-left").hide();
-        $(".sb_text").hide();
-        $(".slide-right").show();
-    });
+          $(".slide-left").click(function(){
+              $(".leftSideBar").animate({
+                  width: "5%"
+              });
+              $(".outlogin").animate({
+                  width: "30%"
+              });
+              $(".leftMainWrap").animate({
+                  width: "95%"
+              });
+              $(".slide-left").hide();
+              $(".sb_text").hide();
+              $(".slide-right").show();
+          });
 
-    $(".slide-right").click(function(){
-        $(".box").animate({
-            width: "16.66666667%"
-        });
-        $(".outlogin").animate({
-            width: "80%"
-        });
-        $(".slide-right").hide();
-        $(".sb_text").fadeIn('slow').animate({opacity: 1.0});
-        $(".slide-left").show();
-    });
-
-});
+          $(".slide-right").click(function(){
+              $(".leftMainWrap").animate({
+                  width: "83%"
+              });
+              $(".leftSideBar").animate({
+                  width: "17%"
+              });
+              $(".outlogin").animate({
+                  width: "80%"
+              });
+              $(".slide-right").hide();
+              $(".sb_text").fadeIn('slow').animate({opacity: 1.0});
+              $(".slide-left").show();
+          });
+      });
 // End of Global Left Navigator full screen sidebar Collapsible function//
 
 //Global  Select Element Search function//
